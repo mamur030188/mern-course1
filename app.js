23f79d48ca19e9899a63c1ac4bd847d29ba7e6fb
Mamirjon Mamadaliev <mike030188@gmail.com>
2021-07-16 00:05:42 +0900

    feat: Connected to MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const express = require ('express');
-const config = require ('config');
+const express = require('express');
+const config = require('config');
 const app = express();
+const mongoose = require('mongoose');  
 
 const PORT = config.get('port') || 5000
-app.listen(5000, () => console.log(`App has been started on port ${PORT}...`))
+
+async function start() {
+    try {
+       await mongoose.connect(config.get('mongoUri'), {
+           // Following 3 properties helps to good connection MongoDB //
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+       })
+       app.listen(5000, () => console.log(`App has been started on port ${PORT}...`))
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
+

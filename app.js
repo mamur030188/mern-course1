51b30c27866c21d0c0230a35fe687efe81d5dabd
Mamirjon Mamadaliev <mike030188@gmail.com>
2021-07-15 22:48:17 +0900

    BRR: Server has been Set Up

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require ('express');
+const config = require ('config');
+const app = express();
+
+const PORT = config.get('port') || 5000
+app.listen(5000, () => console.log(`App has been started on port ${PORT}...`))
